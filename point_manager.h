ddf540011563bf26ddb1ce424bb461ab027e4520
soffochka100 <148487087+soffochka100@users.noreply.github.com>
2024-12-25 12:59:42 +0300

    Update point_manager.h

--- point_manager.h
+++ point_manager.h
@@ -2,9 +2,8 @@
 #include "point.h"
 namespace singletones {
 	struct PointManager{
-		static double get_distance(abstracts::Point& a, abstracts::Point& b); // Функция для вычисления расстояния между двумя точками
-		static bool equal(abstracts::Point& a, abstracts::Point& b); // Функция для проверки равенства двух точек
-		static abstracts::Point new_point(abstracts::Point& a, abstracts::Point& b, double speed_kmh); // Функция для создания новой точки на основе двух существующих
-    // и заданной скорости в км/ч
+		static double get_distance(abstracts::Point& a, abstracts::Point& b);
+		static bool equal(abstracts::Point& a, abstracts::Point& b);
+		static abstracts::Point new_point(abstracts::Point& a, abstracts::Point& b, double speed_kmh);
 	};
 }
