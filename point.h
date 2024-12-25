b17e60497052e8c15e876c3637231786db61ae10
Таисия <148582636+TaisiyaKukosh@users.noreply.github.com>
2024-12-25 19:49:51 +0300

    Update point.h

--- point.h
+++ point.h
@@ -1,14 +1,19 @@
 #pragma once
 #include <fstream>
+#include <string>
+#include "socket.h"
+
+using namespace std;
 
 namespace abstracts {
 
 	struct Point {
-		double x; // Объявляем х,у для задания координат точки
+		double x;
 		double y;
-		void input(); // Метод для ввода координат точки
-		void print();  // Метод для вывода координат точки на экран
-		void save(std::ofstream& os);  // Метод для сохранения координат точки в файл
-		void read(std::ifstream& is); // Метод для чтения координат точки из файла
+        void input(Socket& socket);
+        string print();
+		void save(ofstream& os);
+		void read(ifstream& is);
 	};
 }
+
