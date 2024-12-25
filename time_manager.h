35e3f4b37b737aee476bb92515597f14f54d02d4
Maksim-Pershin <147662806+Maksim-Pershin@users.noreply.github.com>
2024-12-25 12:35:10 +0300

    Update time_manager.h

--- time_manager.h
+++ time_manager.h
@@ -3,22 +3,22 @@
 #include "order.h"
 #include "storage.h"
 #include <vector>
-struct TimeManager{       // Управление временем
-	void time_shift(int hours,       //  кол-во часов для смещения времени
-		std::vector<abstracts::Storage>& storages,
-		std::vector<abstracts::Courier>& couriers,
-		std::vector<abstracts::Order>& orders);
+struct TimeManager{
+	void time_shift(int hours,
+		vector<abstracts::Storage>& storages,
+		vector<abstracts::Courier>& couriers,
+		vector<abstracts::Order>& orders);
 private:
-	void assign_orders(     //  Получает вектор курьеров и заказов, используем для распределения или назначения заказов курьерам
-		std::vector<abstracts::Storage>& storages,
-		std::vector<abstracts::Courier>& couriers,
-		std::vector<abstracts::Order>& orders);
-	void one_hour_move(  //  Принимает вектора курьеров и заказов, симулирует изменения в системе за один часЫ
-		std::vector<abstracts::Storage>& storages,
-		std::vector<abstracts::Courier>& couriers,
-		std::vector<abstracts::Order>& orders);
-	void complete_orders(   // обрабатывет завершение или выполнение заказов
-		std::vector<abstracts::Storage>& storages,
-		std::vector<abstracts::Courier>& couriers,
-		std::vector<abstracts::Order>& orders);
+	void assign_orders(
+		vector<abstracts::Storage>& storages,
+		vector<abstracts::Courier>& couriers,
+		vector<abstracts::Order>& orders);
+	void one_hour_move(
+		vector<abstracts::Storage>& storages,
+		vector<abstracts::Courier>& couriers,
+		vector<abstracts::Order>& orders);
+	void complete_orders(
+		vector<abstracts::Storage>& storages,
+		vector<abstracts::Courier>& couriers,
+		vector<abstracts::Order>& orders);
 };
